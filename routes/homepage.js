fda7b3b97c4f88b39ec28aa406dcb72cd6b998ed
suyashjawale <suyashjawale245@gmail.com>
2022-08-06 08:00:30 +0530

    push

--- routes/homepage.js
+++ routes/homepage.js
@@ -7,13 +7,15 @@ var moment = require('moment-timezone');
 var axios = require('axios');
 
 function run_loop(element, callback) {
-    let sql = `select m.*,name from maintenance m, members me where m.maintenance_flat_no=me.flat_no and maintenance_month_id= ${connection.escape(element.month_id)}; select * from earnings where earning_month=${connection.escape(element.month_id)}; select * from expenses where expense_month=${connection.escape(element.month_id)}; select * from savings where saving_month_id=${connection.escape(element.month_id)}`
+    let sql = `select m.*,name from maintenance m, members me where m.maintenance_flat_no=me.flat_no and maintenance_month_id= ${connection.escape(element.month_id)}; select * from earnings where earning_month=${connection.escape(element.month_id)}; select * from expenses where expense_month=${connection.escape(element.month_id)}; select * from savings where saving_month_id=${connection.escape(element.month_id)}; select created_date from months where month_id = ${connection.escape(element.month_id)}`
+    
     let month = { key: element.month_id, month_name: element.month_name }
     connection.query(sql, (err, result, fields) => {
         month.maintenance = result[0];
         month.earnings = result[1];
         month.expenses = result[2];
         month.savings = result[3][0];
+        month.date = result[4][0]['created_date'];
         callback(month)
     })
 }
@@ -38,7 +40,7 @@ router.get('/', (req, res) => {
 
     client("JSON.SET",[ipAddress, "$", JSON.stringify(visitors)])
 
-    let sql = "select * from months order by created_date desc;"
+    let sql = "select * from months;"
     let month = []
 
     connection.query(sql, (error, results, fields) => {
@@ -51,7 +53,7 @@ router.get('/', (req, res) => {
                     month.push(result)
                     processed++;
                     if (processed == arr.length)
-                        res.render('index', { "data": month })
+                        res.render('index', { "data": month.sort((x, y) => y.date - x.date) })
                 })
             })
         }
